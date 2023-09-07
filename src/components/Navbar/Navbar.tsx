24655afde7e2f68ffdec6efedc7453f1b7152049
Tomas Grusz <grusztomas0@gmail.com>
2023-09-07 18:26:15 +0200

    Update Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,25 +1,38 @@
-import './Navbar.scss';
+import styles from './Navbar.module.scss';
 
-import { BsGithub, BsLinkedin } from 'react-icons/bs';
+import { BsGithub, BsLinkedin, BsPerson } from 'react-icons/bs';
+import { BiHomeAlt2, BiCarousel } from 'react-icons/bi';
+import { MdAlternateEmail } from 'react-icons/md';
 import { Link } from 'react-router-dom';
 
-import './Navbar.scss';
 import { AnimatedLogoSVG } from 'components/common/LogoSVG';
 
 const Navbar = () => {
   return (
-    <div className="Navbar">
-      <div className="logo">
-        <AnimatedLogoSVG size={64} />
+    <div className={styles.Navbar}>
+      <div className={styles.logo}>
+        <AnimatedLogoSVG size={48} />
       </div>
-      <div className="routes">
-        <div className="route">About</div>
-        <div className="route">Portfolio</div>
-        <div className="route">Experience</div>
-        <div className="route">Contact</div>
+      <div className={styles.routes}>
+        <div className={styles.route}>
+          <BiHomeAlt2 className={styles.routeLink} />
+          Home
+        </div>
+        <div className={styles.route}>
+          <BsPerson className={styles.routeLink} />
+          About Me
+        </div>
+        <div className={styles.route}>
+          <BiCarousel className={styles.routeLink} />
+          Portfolio
+        </div>
+        <div className={styles.route}>
+          <MdAlternateEmail className={styles.routeLink} />
+          Contact Me
+        </div>
       </div>
-      <div className="links">
-        <div className="linkedin">
+      <div className={styles.links}>
+        <div className={styles.linkedin}>
           <Link
             to={'https://www.linkedin.com/in/tomasgrusz/'}
             target="_blank"
@@ -28,7 +41,7 @@ const Navbar = () => {
             <BsLinkedin />
           </Link>
         </div>
-        <div className="github">
+        <div className={styles.github}>
           <Link to={'https://github.com/tomasgrusz'} target="_blank" rel="noopener noreferrer">
             <BsGithub />
           </Link>
