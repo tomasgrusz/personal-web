570224eeb1a8cb0a6398966851f7b5752449306c
Tomas Grusz <grusztomas0@gmail.com>
2023-10-06 14:04:48 +0200

    Convert moon image to .webp

--- src/components/Moon/Moon.tsx
+++ src/components/Moon/Moon.tsx
@@ -5,7 +5,7 @@ const Moon = () => {
     <div
       className={styles.moon}
       style={{
-        backgroundImage: `radial-gradient(rgba(114, 57, 171, 0.5) 0%, rgba(114, 57, 171, 0.5) 100%), radial-gradient(rgba(200,200,200, 0.35) 0%, rgba(200,200,200, 0.55) 70%, rgba(200,200,200, 0.85) 100%), url(/images/moon.png)`,
+        backgroundImage: `radial-gradient(rgba(114, 57, 171, 0.5) 0%, rgba(114, 57, 171, 0.5) 100%), radial-gradient(rgba(200,200,200, 0.35) 0%, rgba(200,200,200, 0.55) 70%, rgba(200,200,200, 0.85) 100%), url(/images/moon.webp)`,
         boxShadow: '0 2px 80px 12px rgb(163, 122, 195), inset 16px -8px 16px 10px rgba(70, 40, 70)',
       }}
     ></div>
