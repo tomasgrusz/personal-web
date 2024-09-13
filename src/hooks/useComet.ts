f6935985b85ca38c3670dcdc19712a1bd41b9eb6
tomasgrusz <tomas@grusz.dev>
2024-09-13 14:45:23 +0200

    fix: comet buildup on tab not visible

--- src/hooks/useComet.ts
+++ src/hooks/useComet.ts
@@ -111,12 +111,14 @@ const useComet = (containerRef: RefObject<HTMLDivElement>) => {
 
       // Function to create a new comet every 2.5 to 7.5 seconds
       const loopComet = () => {
-        // Randomize comet's initial position
-        const initialX = Math.floor(Math.random() * (containerElement.offsetWidth - 20)); // Adjust the comet's width
-        // Randomize comet's target position
-        const targetX = Math.floor(Math.random() * (containerElement.offsetWidth - 20)); // Adjust the comet's width
-        // Initialize comet creation
-        createComet(initialX, targetX - DEFAULT_TAIL);
+        if (document.visibilityState === 'visible') {
+          // Randomize comet's initial position
+          const initialX = Math.floor(Math.random() * (containerElement.offsetWidth - 20)); // Adjust the comet's width
+          // Randomize comet's target position
+          const targetX = Math.floor(Math.random() * (containerElement.offsetWidth - 20)); // Adjust the comet's width
+          // Initialize comet creation
+          createComet(initialX, targetX - DEFAULT_TAIL);
+        }
         // wait for a random time before creating the next comet
         setTimeout(loopComet, 2500 + Math.random() * 5000);
       }
