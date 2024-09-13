c052da8a4cd77c01f78a3b966d6e0f9b013be0e2
tomasgrusz <tomas@grusz.dev>
2024-09-13 14:47:46 +0200

    feat: add different moons based on special events

--- src/components/Moon/Moon.tsx
+++ src/components/Moon/Moon.tsx
@@ -1,14 +1,30 @@
 import styles from './Moon.module.scss';
 
+const calculateMoon = () => {
+  const today = new Date();
+  const isHalloween = today.getMonth() === 9 && today.getDate() === 31; // October is 9th month
+  const isFridayThe13th = today.getDate() === 13 && today.getDay() === 5; // Friday is 5th day of the week
+  const isChristmas = today.getMonth() === 11 && today.getDate() >= 22 && today.getDate() <= 31; // December is 11th month
+
+  if (isHalloween) return 'pumpkinMoon';
+  if (isFridayThe13th) return 'bloodMoon';
+  if (isChristmas) return 'frostMoon';
+  return 'normalMoon';
+}
+
 const Moon = () => {
+  const moonType = calculateMoon();
   return (
-    <div
-      className={styles.moon}
-      style={{
-        backgroundImage: `radial-gradient(rgba(114, 57, 171, 0.5) 0%, rgba(114, 57, 171, 0.5) 100%), radial-gradient(rgba(200,200,200, 0.35) 0%, rgba(200,200,200, 0.55) 70%, rgba(200,200,200, 0.85) 100%), url(/images/moon.webp)`,
-        boxShadow: '0 2px 80px 12px rgb(163, 122, 195), inset 16px -8px 16px 10px rgba(70, 40, 70)',
-      }}
-    ></div>
+    <div className={`${styles.moon} ${styles[moonType]}`}>
+      {moonType !== 'normalMoon' && 
+        <p className={styles.specialMoonText}>
+          It's{' '}
+          {moonType === 'pumpkinMoon' && 'Halloween! 🎃'}
+          {moonType === 'bloodMoon' && 'Friday the 13th! 🩸'}
+          {moonType === 'frostMoon' && 'Christmas time! ❄️'}
+        </p>
+      }
+    </div>
   );
 };
 
